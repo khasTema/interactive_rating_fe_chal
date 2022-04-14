c1cadd73ac9d12a98fdfc77303038e12ff44c7e9
Artem Khasin <akhasin.dev@gmail.com>
2022-04-14 13:48:18 +0300

    Readme chamged

--- src/pages/Prompt.js
+++ src/pages/Prompt.js
@@ -11,8 +11,8 @@ export default function Prompt(props){
             <div className="star_img">
                 <img src={Icon} alt="start-icon" />
             </div>
-            <h1 className="card_headding">How did we do?</h1>
-            <p className="card_description">Please let us know how we did with your support request. All feedback is appreciated 
+            <h1 className="card_headding align">How did we do?</h1>
+            <p className="card_description align">Please let us know how we did with your support request. All feedback is appreciated 
             to help us improve our offering!</p>
             <div className="rating">
                 {Array.apply(0, Array(5)).map(function (x, i) {
